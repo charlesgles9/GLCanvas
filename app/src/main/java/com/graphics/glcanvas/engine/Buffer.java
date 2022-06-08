ea7bacd589c51073ba66df5742a35375b41995d5
tnt <charlesgles9@gmail.com>
2022-06-08 14:28:22 +0300

    organized the batched renderer

---
+++ app/src/main/java/com/graphics/glcanvas/engine/Buffer.java
@@ -0,0 +1,32 @@
+package com.graphics.glcanvas.engine;
+
+import android.opengl.GLES32;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+public class Buffer {
+
+    static FloatBuffer createFloatBuffer(int buffer,int position,float[]data){
+        int BYTES_PER_FLOAT = 4;
+        ByteBuffer bb=ByteBuffer.allocateDirect(data.length* BYTES_PER_FLOAT);
+        FloatBuffer floatBuffer=bb.order(ByteOrder.nativeOrder()).asFloatBuffer();
+        floatBuffer.put(data);
+        floatBuffer.position(position);
+        GLES32.glBindBuffer(GLES32.GL_ARRAY_BUFFER,buffer);
+        GLES32.glBufferData(GLES32.GL_ARRAY_BUFFER,floatBuffer.capacity()* BYTES_PER_FLOAT,null,GLES32.GL_DYNAMIC_DRAW);
+        return floatBuffer;
+    }
+
+    static ShortBuffer createDrawListBuffer( int position, short[]data){
+        int BYTES_PER_SHORT = 2;
+        ByteBuffer dlb=ByteBuffer.allocateDirect(data.length* BYTES_PER_SHORT);
+        dlb.order(ByteOrder.nativeOrder());
+        ShortBuffer drawListBuffer=dlb.asShortBuffer();
+        drawListBuffer.put(data);
+        drawListBuffer.position(position);
+        return drawListBuffer;
+    }
+}
